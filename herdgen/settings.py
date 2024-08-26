dc4e55106ee68c0b75c41db7fc9626a7d8265f3b
Owen-Dechow <owen.dechow@gmail.com>
2024-08-25 21:17:22 -0400

    Update settings

--- herdgen/settings.py
+++ herdgen/settings.py
@@ -149,7 +149,7 @@ if env("TERMINAL_EMAIL", bool):
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    EMAIL_HOST = "smtp.google.com"
+    EMAIL_HOST = "smtp.gmail.com"
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
     EMAIL_HOST_USER = env("EMAIL_HOST_USER", str)
