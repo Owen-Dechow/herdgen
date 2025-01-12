2f0a0b37d1bcdd0ecb062a35d74a4b57a75bc57e
Owen Dechow <owen.dechow@gmail.com>
2025-01-12 15:25:32 -0500

    422 means

--- scripts/add_pta_and_dam_only_phenotypes.py
+++ scripts/add_pta_and_dam_only_phenotypes.py
@@ -1,16 +1,17 @@
 from base.traitsets.traitset import Traitset
 from .add_pta_visibility_defaults import add_pta_visibility_defaults
-from base.models import Animal, Class
+from base.models import Animal
 
 
 def add_pta_and_dam_only_phenotypes():
     add_pta_visibility_defaults()
-    for klass in Class.objects.all():
-        klass.recalculate_ptas()
 
     traitsets = {}
-    animals = Animal.objects.select_related("connectedclass").filter(herd=None)
-    for anim in animals:
+    animals = Animal.objects.select_related("connectedclass").all()
+    count = Animal.objects.count()
+    print(f"{(count)} Animals to process")
+
+    for idx, anim in enumerate(animals.iterator(chunk_size=50)):
         if anim.connectedclass_id in traitsets:
             traitset = traitsets[anim.connectedclass_id]
         else:
@@ -19,4 +20,5 @@ def add_pta_and_dam_only_phenotypes():
 
         anim.ptas = traitset.derive_ptas_from_genotype(anim.genotype, 0, 0)
 
-    Animal.objects.bulk_update(animals, ["ptas"])
+        anim.save()
+        print(f"Animal {idx + 1}/{count} {(idx - 1)/count * 100}%")
